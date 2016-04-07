1d74c570922d21914ccbaf548d05dcf3c57db047
qinm <499259430@qq.com>
2016-04-07 20:52:45 +0800

    add merge_sort and quick_sort

---
+++ sort/merge_sort/merge_sort.cpp
@@ -0,0 +1,35 @@
+
+/*	归并排序，其基本思路就是将数组分成二组A，B，如果这二组组内的数据都是有序的，那么就可以很方便的将这二组数据进行排序。
+*	如A为1、5、9，B为3、6、8，则合并后为1、3、5、6、8、9
+*	如何让这二组组内数据有序了？可以将A，B组各自再分成二组。
+*	当分出来的小组只有一个数据时，可以认为这个小组组内已经达到了有序，然后再合并相邻的二个小组就可以了。
+*	这样通过先递归的分解数列，再合并数列就完成了归并排序。
+*/
+
+
+
+
+void merge(vector<int>& nums,int start,int end,int mid,vector<int>& temp){			//此时nums[start...mid] 与 nums[mid+1...end]两部分已分别有序，利用temp数组将两部分合并，则合并后nums[start...end]有序
+	int i=start,j=mid+1;
+	int k=0;
+	while(i<=mid&&j<=end){			
+		if(nums[i]<=nums[j]){
+			temp[k++]=nums[i];
+		}else{
+			temp[k++]=nums[j];
+		}
+	}
+	while(i<=mid)	temp[k++]=nums[i];
+	while(j<=end)	temp[k++]=nums[j];
+	fot(int i=0;i<k;++i){
+		nums[i+start]=temp[i];
+	}
+}
+
+void mergeSort(vector<int>& nums,int start,int end,vector<int>& temp){
+	if(start>=end)	return;
+	int mid=start+(end-start)/2;			//不断二分，递归进行归并排序
+	mergeSort(nums,start,mid,temp);			//左边有序
+	mergeSort(nums,mid+1,end,temp);			//右边有序
+	merge(nums,start,end,mid,temp);			//合并左右两部分
+}
---
+++ sort/quick_sort/quick_sort.cpp
@@ -0,0 +1,51 @@
+/*		
+*		快排的基本思想是：
+*		1）首先选定一个元素作为主元，一般选择最左或最右元素作为主元
+*		2）将数组中比主元小的元素移动到主元的左边，比主元大的元素移动到主元的右边
+*		3）分别对主元左、右两部分元素继续上述操作
+*/
+
+
+
+/*  快速排序的代码非常简单，首先调用partion过程，得到主元元素在一次排序后的下标，然后对左、右子数组分别递归调用quick_sort()  */
+void quick_sort(vector<int>& nums,int start,int end){
+	if(start<end){
+		int mid=partion2(nums,start,end);
+		quick_sort(nums,start,mid-1);
+		quick_sort(nums,mid+1,end);
+	}
+}
+
+/*可以发现快速排序的关键是partion过程*/
+int partion(vector<int>& nums,int start,int end){                //算法导论上的方案，以末尾元素作为主元，从左到右进行调整
+	int pivot=nums[end];
+	int j=start-1;
+	for(int i=start;i<end;++i){
+		if(nums[i]<=pivot){
+			++j;
+			swap(nums[i],nums[j]);
+		}
+	}
+	swap(nums[j+1],nums[end]);
+	return j+1;
+}
+
+int partion2(vector<int>& nums,int start,int end){             //另一种解法：挖坑填数
+	int pivot=nums[start];
+	int i=start,j=end;
+	while(i<j){
+		while(i<j&&nums[j]>=pivot)	--j;
+		if(i<j){
+			nums[i]=nums[j];
+			++i;
+		}
+		while(i<j&&nums[i]<pivot)	++i;
+		if(i<j){
+			nums[j]=nums[i];
+			--j;
+		}
+	}
+	nums[j]=pivot;
+	return j;
+}
+
